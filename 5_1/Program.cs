f0b4bde1ef9dcd84c6eaa2a63f620042c5a7219a
child-sawko <casha.morozov.2015@mail.ru>
2022-06-22 11:33:10 +0300

    1

---
+++ 5_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\user\Desktop\Dir";
+            string subpath = @"Folder_";
+
+            for (int i =0; i < 100; i++)
+            {
+                Directory.CreateDirectory($"{path}/{subpath+i}");
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                Directory.Delete($"{path}/{subpath + i}");
+            }
+        }
+    }
+}
---
+++ 5_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_2
+{
+    class Program
+    {
+        static string path = @"\Users\user\Desktop\Dir\File.txt";
+        static void Main(string[] args)
+        {
+            using (FileStream fs = File.Create(path)) { }
+
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                    sw.WriteLine("Не отчисляйте!");
+            }
+            
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                    Console.WriteLine(sr.ReadLine());
+            }
+            Console.ReadLine();
+        }
+    }
+}
