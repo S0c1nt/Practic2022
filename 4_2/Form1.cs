f851698fb9ff8ba2aa14d0900e42d2f16cc05b3e
child-sawko <casha.morozov.2015@mail.ru>
2022-06-22 09:45:40 +0300

    1

---
+++ 4_2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int factorial(int n)
+        {
+            if (n <= 1)
+                return 1;
+            else
+                return (n * factorial(n - 1));
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text);
+            label3.Text = Convert.ToString(factorial(n));
+        }
+    }
+}
---
+++ 4_3/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            if (radioButton1.Checked == true)
+            {
+                textBox3.Text = Convert.ToString(a % b);
+            }
+            if (radioButton2.Checked == true)
+            {
+                textBox3.Text = Convert.ToString(Math.Pow(a, b));
+            }
+            if (radioButton3.Checked == true)
+            {
+                textBox3.Text = Convert.ToString(a) + Convert.ToString(b);
+            }
+            if (radioButton4.Checked == true)
+            {
+                textBox3.Text = Convert.ToString(a/b);
+            }
+        }
+    }
+}
